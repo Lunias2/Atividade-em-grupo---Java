c9f14afc51236d37146b2fbbccb5cdf64b56e784
Aluno <Aluno@192.168.200.11>
2023-03-27 21:04:37 -0300

    Implementado Uso de Interface grafica basica.

---
+++ Atividade2 Grupo/Atividade2/src/jogo/CapturaIp.java
@@ -0,0 +1,16 @@
+package jogo;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class CapturaIp {
+	
+	public static String capturarIP() throws UnknownHostException {
+		String ip = "";
+		InetAddress address1 = InetAddress.getLocalHost();
+		
+		
+		return ip;
+		
+	}
+}
